21f8bb9e5acc744532bc4aa6031e7fba3db8d3fc
lkhdtu1 <boygamer942@gmail.com>
2025-11-23 11:48:14 +0100

    modification du fichier Main.java

--- src/atelier_TAMIS_MOHAMMED/Main.java
+++ src/atelier_TAMIS_MOHAMMED/Main.java
@@ -3,7 +3,7 @@ package atelier_TAMIS_MOHAMMED;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello, I'm TAMIS MOHAMMED");
 
 	}
 
